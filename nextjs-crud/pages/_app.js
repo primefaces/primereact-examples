ddf2bc88ed3363320fe4e9bce9b1b50900188760
Cagatay Civici <cagatay.civici@gmail.com>
2021-11-04 17:42:47 +0300

    Nextjs with crud

---
+++ nextjs-crud/pages/_app.js
@@ -0,0 +1,11 @@
+import '../styles/globals.css';
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp

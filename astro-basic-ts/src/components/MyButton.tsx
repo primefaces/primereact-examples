147cb1ce4010959c4693b1372b8efe929f598c4d
HummingMind <87399963+HummingMind@users.noreply.github.com>
2022-12-20 14:48:07 -0600

    Moved the PrimeReact CSS imports from the component itself to the PrimeIndex page

--- astro-basic-ts/src/components/MyButton.tsx
+++ astro-basic-ts/src/components/MyButton.tsx
@@ -1,7 +1,4 @@
 import { Button } from "primereact/button"
-import "primereact/resources/themes/lara-light-indigo/theme.css";
-import "primereact/resources/primereact.min.css";
-import "primeicons/primeicons.css";
 
 export default function MyButton() {
 

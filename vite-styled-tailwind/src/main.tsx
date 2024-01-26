a0e99ad019371b9ce0edce7b2d089dd38cc44414
Melloware <mellowaredev@gmail.com>
2024-01-26 13:14:25 -0500

    Update main.tsx

--- vite-styled-tailwind/src/main.tsx
+++ vite-styled-tailwind/src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { PrimeReactProvider } from 'primereact/api';
+import Tailwind from 'primereact/passthrough/tailwind';
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <PrimeReactProvider value={{ unstyled: true, pt: Tailwind }}>
+      <App />
+    </PrimeReactProvider>
   </React.StrictMode>
-)
+);

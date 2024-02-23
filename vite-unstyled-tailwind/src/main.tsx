1747c24839d88f9fe1d3ac12ddd97282a9efe4ea
Melloware <mellowaredev@gmail.com>
2024-02-23 15:26:55 -0500

    Tailwind Merge (#26)

--- vite-unstyled-tailwind/src/main.tsx
+++ vite-unstyled-tailwind/src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { PrimeReactProvider } from "primereact/api";
 import Tailwind from "primereact/passthrough/tailwind";
+import { twMerge } from "tailwind-merge";
 import App from "./App";
 // tailwind css
 import "./index.css";
@@ -11,7 +12,7 @@ import "./App.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <PrimeReactProvider value={{ unstyled: true, pt: Tailwind }}>
+    <PrimeReactProvider value={{ unstyled: true, pt: Tailwind, ptOptions: { mergeSections: true, mergeProps: true, classNameMergeFunction: twMerge } }}>
       <App />
     </PrimeReactProvider>
   </React.StrictMode>

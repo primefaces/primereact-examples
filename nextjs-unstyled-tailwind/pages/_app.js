a9227c0e6e5852c6bc5a4a22d77efeae1965bc08
melloware <mellowaredev@gmail.com>
2024-02-23 17:29:40 -0500

    PR 10.5.1

--- nextjs-unstyled-tailwind/pages/_app.js
+++ nextjs-unstyled-tailwind/pages/_app.js
@@ -1,12 +1,13 @@
 import { PrimeReactProvider } from "primereact/api";
 import Tailwind from "primereact/passthrough/tailwind";
 import { ThemeSwitcher } from "../components/ThemeSwitcher";
+import { twMerge } from "tailwind-merge";
 import "../styles/globals.css";
-import 'primeicons/primeicons.css';
+import "primeicons/primeicons.css";
 
 export default function App({ Component, pageProps }) {
   return (
-    <PrimeReactProvider value={{ unstyled: true, pt: Tailwind }}>
+    <PrimeReactProvider value={{ unstyled: true, pt: Tailwind, ptOptions: { mergeSections: true, mergeProps: true, classNameMergeFunction: twMerge } }}>
       <ThemeSwitcher />
       <Component {...pageProps} />
     </PrimeReactProvider>

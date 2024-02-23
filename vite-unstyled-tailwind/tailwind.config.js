440e8cdffa5d7dac4544a11904a352e435454c0c
melloware <mellowaredev@gmail.com>
2024-02-23 15:26:37 -0500

    Tailwind Merge

--- vite-unstyled-tailwind/tailwind.config.js
+++ vite-unstyled-tailwind/tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["src/**/*.{ts,tsx,js,jsx}", "index.html"],
+  content: ["./index.html", "./node_modules/primereact/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};

808dcbc350b73cb6298d41dcfead85313c346435
melloware <mellowaredev@gmail.com>
2024-02-25 10:11:35 -0500

    Update vite example

--- vite-basic-ts/src/main.tsx
+++ vite-basic-ts/src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { PrimeReactProvider } from "primereact/api";
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <PrimeReactProvider>
+       <App />
+    </PrimeReactProvider>
   </React.StrictMode>
 )

6a327bcac031120afc00b31c30e9069f8d38a5f8
melloware <mellowaredev@gmail.com>
2024-04-11 10:52:49 -0400

    NextJS 14

--- nextjs-styled-tailwind/pages/_app.js
+++ nextjs-styled-tailwind/pages/_app.js
@@ -2,7 +2,6 @@ import { PrimeReactProvider } from "primereact/api";
 
 import "../styles/globals.css";
 import "primereact/resources/themes/lara-light-blue/theme.css";
-import "primereact/resources/primereact.min.css";
 
 export default function App({ Component, pageProps }) {
   return (

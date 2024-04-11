106a6ee94f9e02bb24c4b8a3bc826347acd89a17
Melloware <mellowaredev@gmail.com>
2024-04-11 10:53:24 -0400

    NextJS 14 (#31)

--- nextjs-styled-tailwind/pages/index.js
+++ nextjs-styled-tailwind/pages/index.js
@@ -1,11 +1,32 @@
+import { useState } from "react";
 import { Button } from "primereact/button";
+import { InputText } from "primereact/inputtext";
 
 export default function Home() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="p-5 h-screen bg-gray-900 flex items-center justify-center">
+    <>
+      <p className="mb-10">
+        <a href="https://nextjs.org/" target="_blank">
+          <img src="/next.svg" className="logo inline-block" alt="Next.JS logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src="/react.svg" className="logo react inline-block" alt="React logo" />
+        </a>
+      </p>
+      <h1>Next.js + PrimeReact + TailwindCSS</h1>
       <div>
-        <Button label="Click" className="text-white" />
+        <h2>Demo app showing PrimeReact + Tailwind CSS in unstyled mode</h2>
       </div>
-    </div>
+      <div className="card">
+        <Button icon="pi pi-plus" className="mr-2" label="Increment" onClick={() => setCount((count) => count + 1)}></Button>
+        <InputText value={count.toString()} />
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">Click on the Next and React logos to learn more</p>
+    </>
   );
 }
